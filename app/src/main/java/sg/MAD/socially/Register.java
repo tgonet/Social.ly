17a4599a4f52091eebffdf0f2d9df506ce977780
Terris Ng <s10197829@connect.np.edu.sg>
2020-05-29 01:11:11 +0800

    Register Validation

--- app/src/main/java/sg/MAD/socially/Register.java
+++ app/src/main/java/sg/MAD/socially/Register.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Register extends AppCompatActivity {
 
@@ -26,20 +28,29 @@ public class Register extends AppCompatActivity {
         Continuebutton = findViewById(R.id.Continue_register);
 
         Log.v("test", "testinggggg");
-        if(Name.getText().toString() != null){
 
-            Continuebutton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(Register.this,Register2.class);
-                    intent.putExtra("Name",Name.getText().toString());
-                    intent.putExtra("Password",Password.getText().toString());
-                    intent.putExtra("Email",Email.getText().toString());
-                    intent.putExtra("DOB",DOB.getText().toString());
+        Continuebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = Name.getText().toString();
+                String password = Password.getText().toString();
+                String email = Email.getText().toString();
+                String dob = Email.getText().toString();
+
+
+                if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password) || TextUtils.isEmpty(name) || TextUtils.isEmpty(dob)) {  //Prevent registering if fields are empty
+                    Toast.makeText(Register.this, "All fields required", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Intent intent = new Intent(Register.this, Register2.class);
+                    intent.putExtra("Name", Name.getText().toString());
+                    intent.putExtra("Password", Password.getText().toString());
+                    intent.putExtra("Email", Email.getText().toString());
+                    intent.putExtra("DOB", DOB.getText().toString());
                     startActivity(intent);
                 }
-            });
-        }
+            }
+        });
 
 
     }
