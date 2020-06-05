9399c0f18a182e2a1f2cdbace0cc8534da3e61e2
Terris Ng <s10197829@connect.np.edu.sg>
2020-06-05 21:40:40 +0800

    Birthday Date Calendar Default profile image Camera image

--- app/src/main/java/sg/MAD/socially/Register.java
+++ app/src/main/java/sg/MAD/socially/Register.java
@@ -2,6 +2,7 @@ package sg.MAD.socially;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -9,8 +10,11 @@ import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class Register extends AppCompatActivity {
 
@@ -29,6 +33,36 @@ public class Register extends AppCompatActivity {
         Continuebutton = findViewById(R.id.Continue_register);
 
         Log.v("test", "testinggggg");
+        DOB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*Pick data using data picker*/
+                Calendar c = Calendar.getInstance();
+                int mYear ;
+                int mMonth ;
+                int mDay;
+                mYear = c.get(Calendar.YEAR);
+                mMonth = c.get(Calendar.MONTH);
+                mDay = c.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(Register.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        int y=year;
+                        /*Calender object starts month with 0 so month+1 is needed to get current month*/
+                        int m=month+1;
+                        int d=dayOfMonth;
+                        /*set date to edit text*/
+                        DOB.setText(d+"/"+ m +"/" + year);
+                    }
+                }, mYear, mMonth, mDay);
+
+                /*If you want to use minimum date to select from put setMinData(with the date)*/
+                /*setMaxDate() is you dialog show the max date System.currentTimeMillis() is for current date*/
+                datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+                datePickerDialog.show();
+            }
+        });
 
         Continuebutton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,7 +70,7 @@ public class Register extends AppCompatActivity {
                 String name = Name.getText().toString();
                 String password = Password.getText().toString();
                 String email = Email.getText().toString();
-                String dob = Email.getText().toString();
+                String dob = DOB.getText().toString();
 
                 if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password) || TextUtils.isEmpty(name) || TextUtils.isEmpty(dob)) {  //Prevent registering if fields are empty
                     Toast.makeText(Register.this, "All fields required", Toast.LENGTH_SHORT).show();
