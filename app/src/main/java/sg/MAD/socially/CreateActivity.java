3013f21ebe0c86ee6c81a2f049ac923c8089a460
Ming Zhe Tan <s10193694@connect.np.edu.sg>
2020-06-05 23:53:00 +0800

    Helping joey cuz she is dying

--- app/src/main/java/sg/MAD/socially/CreateActivity.java
+++ app/src/main/java/sg/MAD/socially/CreateActivity.java
@@ -1,6 +1,8 @@
 package sg.MAD.socially;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -17,6 +19,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
