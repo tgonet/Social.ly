ce6392d01d289c546cc19e1dd946e0e87bc84262
Ming Zhe Tan <s10193694@connect.np.edu.sg>
2020-06-07 14:41:42 +0800

    Fixed the default profile pic upload

--- app/src/main/java/sg/MAD/socially/Register2.java
+++ app/src/main/java/sg/MAD/socially/Register2.java
@@ -303,7 +303,21 @@ public class Register2 extends AppCompatActivity {
                         Uri downloadUri = task.getResult();
                         mUri = downloadUri.toString();
 
+                        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                .setDisplayName(Name)
+                                .setPhotoUri(Uri.parse(mUri))
+                                .build();
 
+                        FirebaseUser firebaseUser = auth.getCurrentUser();
+                        firebaseUser.updateProfile(profileUpdates)
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()) {
+                                            Log.d("Checck", "User profile updated.");
+                                        }
+                                    }
+                                });
                         FirebaseUser fuser = auth.getCurrentUser();
                         reference = FirebaseDatabase.getInstance().getReference("Users").child((fuser.getUid()));
                         HashMap<String, Object> map = new HashMap<>();
